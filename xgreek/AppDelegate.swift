bc4f8e8dce8e4db24a13fc975530c20a47438cec
Min Chu <minchu7@yahoo.com>
2019-01-03 17:37:55 -0500

    1.1.4

--- xgreek/AppDelegate.swift
+++ xgreek/AppDelegate.swift
@@ -7,18 +7,32 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        registerForPushNotifications()
         return true
     }
-
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data -> String in
+            return String(format: "%02.2hhx", data)
+        }
+        
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+    func application(_ application: UIApplication,
+                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -40,7 +54,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            print("Notification settings: \(settings)")
+            guard settings.authorizationStatus == .authorized else { return }
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+    }
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+            (granted, error) in
+            print("Permission granted: \(granted)")
+            
+            guard granted else { return }
+            self.getNotificationSettings()
+        }
+    }
+    
 }
 
--- xgreek/ViewController.swift
+++ xgreek/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 import WebKit
 import AudioToolbox
+import AVFoundation
 
 class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler{
     
     var webView: WKWebView!
+    var synthesizer: AVSpeechSynthesizer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.synthesizer = AVSpeechSynthesizer()
+        //self.synthesizer.delegate = self
         
         //javascript interface
         let contentController = WKUserContentController()
         contentController.add( self, name: "vibrateAction")
-        
+        contentController.add( self, name: "speakAction")
+
         let config = WKWebViewConfiguration()
         config.userContentController = contentController
         let rec = self.view.bounds
@@ -49,9 +55,57 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler{
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         if message.name == "vibrateAction" {
             AudioServicesPlayAlertSound(kSystemSoundID_Vibrate);
+        }else if message.name == "speakAction" {
+            if #available(iOS 10.0, *) {
+                do {
+                    try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                    try AVAudioSession.sharedInstance().setActive(true)
+                } catch {
+                    print(error)
+                }
+            }
+            var messageBody = message.body as? String ?? ""
+            messageBody = replaceNoSoundWord( messageBody)
+            let utterance = AVSpeechUtterance(string: messageBody)
+            let v = AVSpeechSynthesisVoice(language: "el-GR")
+            //let utterance = AVSpeechUtterance(string: "this English test")
+            //let v = AVSpeechSynthesisVoice(language: "en-US")
+            //print( v.debugDescription);
+            utterance.voice = v
+            utterance.rate = 0.25
+            utterance.volume = 1
+            if synthesizer.isSpeaking {
+                synthesizer.stopSpeaking(at: .immediate)
+            }
+            DispatchQueue.main.async {
+                self.synthesizer.speak(utterance)
+            }
         }
     }
-    
+    func replaceNoSoundWord(_ inStr:String)->String{
+        let key = "υιου", toW = "γιου"
+        var string = inStr
+        while let idx = string.index(of:key){
+            var newStr = ""
+            if idx.encodedOffset > 0 {
+                if string[idx.encodedOffset-1] != " " {
+                    return inStr
+                }
+                newStr = "\(string[..<idx])" + toW
+            }else{
+                newStr = toW
+            }
+            let endIdx = key.count + idx.encodedOffset
+            if  endIdx != string.count{
+                if string[endIdx] != " "{
+                    return  inStr
+                }
+                newStr += "\(string[endIdx ..< string.count])"
+            }
+            string = newStr
+        }
+        return string
+    }
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
     }
@@ -66,4 +120,60 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler{
         }
         return nil
     }
+}
+extension ViewController: AVSpeechSynthesizerDelegate{
+    private func attributedString(from string: String, highlighting characterRange: NSRange) -> String {
+        return string + characterRange.debugDescription
+    }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
+        print( attributedString(from: utterance.speechString, highlighting: characterRange))
+    }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didStart utterance: AVSpeechUtterance) {
+        print( "start:" + utterance.speechString)
+    }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        print( "finish:" + utterance.speechString)
+    }
+}
+extension StringProtocol where Index == String.Index {
+    func index(of string: Self, options: String.CompareOptions = []) -> Index? {
+        return range(of: string, options: options)?.lowerBound
+    }
+    func endIndex(of string: Self, options: String.CompareOptions = []) -> Index? {
+        return range(of: string, options: options)?.upperBound
+    }
+    func indexes(of string: Self, options: String.CompareOptions = []) -> [Index] {
+        var result: [Index] = []
+        var start = startIndex
+        while start < endIndex,
+            let range = self[start..<endIndex].range(of: string, options: options) {
+                result.append(range.lowerBound)
+                start = range.lowerBound < range.upperBound ? range.upperBound :
+                    index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
+        }
+        return result
+    }
+    func ranges(of string: Self, options: String.CompareOptions = []) -> [Range<Index>] {
+        var result: [Range<Index>] = []
+        var start = startIndex
+        while start < endIndex,
+            let range = self[start..<endIndex].range(of: string, options: options) {
+                result.append(range)
+                start = range.lowerBound < range.upperBound ? range.upperBound :
+                    index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
+        }
+        return result
+    }
+    subscript (i: Int) -> Character {
+        return self[index(startIndex, offsetBy: i)]
+    }
+    subscript (bounds: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start..<end])
+    }
+    
 }
